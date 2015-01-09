ddf638fc3f047c3cd492f81e081d2eb002c00e64
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-09 10:32:24 -0500

    Adding a "build-db.sh" shell script

---
+++ database/build-db.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python=${PYTHON}
+if [ -z ${python} ]; then
+    python="python"
+fi
+
+${python} startrek.py "../data/Star Trek Plotlines - Episodes.csv" "../data/Star Trek Plotlines - People.csv" "../data/Star Trek Plotlines - Plots.csv"
