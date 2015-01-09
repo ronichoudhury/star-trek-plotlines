d7f9aba9bd6ea57c4fc3615d0f20f73433489d92
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-09 10:34:42 -0500

    Moved build-db.sh into root directory

--- build-db.sh
+++ build-db.sh
@@ -5,4 +5,5 @@ if [ -z ${python} ]; then
     python="python"
 fi
 
+cd database
 ${python} startrek.py "../data/Star Trek Plotlines - Episodes.csv" "../data/Star Trek Plotlines - People.csv" "../data/Star Trek Plotlines - Plots.csv"
