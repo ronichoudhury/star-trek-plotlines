227193c6d854695c0e1417f74828642637f7dadd
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-13 14:04:34 -0500

    Improvements to basic vis

--- database/index.js
+++ database/index.js
@@ -1,7 +1,17 @@
 window.onload = function () {
-    // parse a spec and create a visualization view
     function parse(spec) {
-      vg.parse.spec(spec, function(chart) { chart({el:"#vis"}).update(); });
+        vg.parse.spec(spec, function(chart) {
+            var view = chart({el:"#vis"})
+                .on("mouseover", function (evt, item) {
+                    console.log(item.datum.data.title);
+                })
+                .on("mouseout", function (evt, item) {
+                    view.update({
+                        props: "enter"
+                    });
+                })
+                .update();
+        });
     }
 
     parse("scatter.json");
