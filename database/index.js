b2564ae37337cae25c3f7b021a55a651dbbb5ee6
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-13 11:01:28 -0500

    Adding a basic Tangelo app

---
+++ database/index.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    // parse a spec and create a visualization view
+    function parse(spec) {
+      vg.parse.spec(spec, function(chart) { chart({el:"#vis"}).update(); });
+    }
+
+    parse("scatter.json");
+};
