98a70935c3787394e8c5f57dac09a8673038be63
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-13 14:53:43 -0500

    Add ability to filter by season to episodes service

--- database/episodes.py
+++ database/episodes.py
@@ -1,11 +1,12 @@
 import datetime
+import json
 import tangelo
 
 from startrek import DBSession
 from startrek import Episode
 
 
-def scatterplot(*fields):
+def scatterplot(*fields, **kw):
     def get_type_converter(field):
         if field == "airdate":
             return lambda airdate: (airdate - datetime.date.fromtimestamp(0)).total_seconds() * 1000.0
@@ -21,7 +22,20 @@ def scatterplot(*fields):
             return lambda x: x
 
     session = DBSession()
-    eps = session.query(Episode)
+
+    seasons = kw.get("seasons")
+    if seasons is None:
+        eps = session.query(Episode)
+    else:
+        try:
+            seasons = json.loads(seasons)
+            if not isinstance(seasons, list):
+                raise ValueError("not a list")
+        except ValueError as e:
+            tangelo.http_status(400, "Bad argument")
+            return {"error": e}
+
+        eps = session.query(Episode).filter(Episode.season.in_(seasons))
 
     if eps.count() > 0:
         ep = eps[0]
@@ -38,7 +52,10 @@ def scatterplot(*fields):
 
 def run(plot_type, **query):
     if plot_type == "scatter":
-        return scatterplot(query.get("x"), query.get("y"), "title")
+        if "seasons" in query:
+            return scatterplot(query.get("x"), query.get("y"), "title", seasons=query.get("seasons"))
+        else:
+            return scatterplot(query.get("x"), query.get("y"), "title")
     else:
         tangelo.http_status(400, "Illegal plot type")
         return {"error": "Illegal plot type: %s" % (plot_type)}
