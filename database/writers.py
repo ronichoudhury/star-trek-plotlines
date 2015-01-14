10beb2744b34377eed9c908512d42d65bc5c3b75
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-14 15:37:10 -0500

    Added barchart showing writer contributions

--- database/writers.py
+++ database/writers.py
@@ -1,10 +1,11 @@
+import json
 import tangelo
 
 from startrek import DBSession
 from startrek import Episode
 
 
-def count_barchart():
+def count_barchart(sort):
     session = DBSession()
 
     count = {}
@@ -19,12 +20,17 @@ def count_barchart():
             if writer not in seen:
                 count[writer] = count.get(writer, 0) + 1
 
-    return [{"name": r.name, "count": count[r]} for r in sorted(count.keys(), key=lambda x: x.id)]
+    results = [{"name": r.name, "count": count[r]} for r in sorted(count.keys(), key=lambda x: x.id)]
+    if sort:
+        results.sort(key=lambda x: x["count"], reverse=True)
+
+    return results
 
 
-def run(plot_type, **query):
+@tangelo.types(sort=json.loads)
+def run(plot_type, sort=False):
     if plot_type == "count":
-        return count_barchart()
+        return count_barchart(sort=sort)
     else:
         tangelo.http_status(400, "Illegal plot type")
         return {"error": "Illegal plot type: %s" % (plot_type)}
